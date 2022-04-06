8a378e41ec230150e59bef6abb4f9deb13484e63
Kunal Kumar <k9801263933@gmail.com>
2022-04-06 15:16:42 +0530

    Update yt_playlist_downloader.py

--- yt_playlist_downloader.py
+++ yt_playlist_downloader.py
@@ -37,28 +37,28 @@ def downloader1():
       yt = YouTube(i)
       ys = yt.streams.get_highest_resolution()
       filename = ys.download()
-      print("threading 1 -->  " + filename.split('/')[-1] + ' Downloaded')
+      print("threading 1 -->  " + filename.split('/')[-1] + ' -> Downloaded')
 
 def downloader2():
     for i in link[1]:
       yt = YouTube(i)
       ys = yt.streams.get_highest_resolution()
       filename = ys.download()
-      print("threading 2 -->  " + filename.split('/')[-1] + ' Downloaded')
+      print("threading 2 -->  " + filename.split('/')[-1] + ' -> Downloaded')
 
 def downloader3():
     for i in link[2]:
       yt = YouTube(i)
       ys = yt.streams.get_highest_resolution()
       filename = ys.download()
-      print("threading 3 -->  " + filename.split('/')[-1] + ' Downloaded')
+      print("threading 3 -->  " + filename.split('/')[-1] + ' -> Downloaded')
 
 def downloader4():
     for i in link[3]:
       yt = YouTube(i)
       ys = yt.streams.get_highest_resolution()
       filename = ys.download()
-      print("threading 4 -->  " + filename.split('/')[-1] + ' Downloaded')
+      print("threading 4 -->  " + filename.split('/')[-1] + ' -> Downloaded')
 
 t1 = threading.Thread(target=downloader1, name='d1')
 t2 = threading.Thread(target=downloader2,name='d2')

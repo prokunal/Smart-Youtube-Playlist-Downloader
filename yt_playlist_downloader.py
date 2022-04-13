9d11cabd9a3cd5cbc17809966ad4690f26c07182
Kunal Kumar <k9801263933@gmail.com>
2022-04-13 15:02:37 +0530

    Update yt_playlist_downloader.py

--- yt_playlist_downloader.py
+++ yt_playlist_downloader.py
@@ -14,21 +14,7 @@ except:
     print('usage: python3 {} url'.format(__file__.split('/')[-1]))
     sys.exit(0)
 #global links
-print("Choose resolution to download.\n1. 480p\n2. 720p\n3. 1080p\n4. 4k")
-choice = input("Select resolution: ")
-if choice == '1':
-    res = "480p"
-elif choice == '2':
-    res = "720p"
-elif choice == '3':
-    res = "1080p"
-elif choice == '4':
-    res = "2160p"
-else:
-    print("Choose correct resolution.")
-    sys.exit(0)
-
-print("\nPlaylist Name : {}\nChannel Name  : {}\nTotal Videos  : {}\nTotal Views   : {}".format(p.title,p.owner,p.length,p.views))
+print("Playlist Name : {}\nChannel Name  : {}\nTotal Videos  : {}\nTotal Views   : {}".format(p.title,p.owner,p.length,p.views))
 links = []
 size = 0
 try:
@@ -37,66 +23,40 @@ try:
 except:
     print('Playlist link is not valid.')
     sys.exit(0)
-    
-def split(links,size):
+size = ceil(len(links)/4)
+def split_link(links,size):
     for i in range(0,len(links),size):
         yield links[i:i+size]
-      
-size = ceil(len(links)/4)
-link = list(split(links,size))
+link = list(split_link(links,size))
 
 print("Downloading Started...\n")
 def downloader1():
     for i in link[0]:
       yt = YouTube(i)
-      for j in yt.streams:
-          check_res = str(j)
-          if  check_res.find(res) > 0:
-              ys = yt.streams.filter(res=res).first()
-              break
-          else:
-              ys = yt.streams.get_highest_resolution()
+      ys = yt.streams.get_highest_resolution()
       filename = ys.download()
-      print("threading 1 -->  " + filename.split('/')[-1] + ' -> Downloaded')
+      print("threading 1 -->  " + filename.split('/')[-1] + ' Downloaded')
 
 def downloader2():
     for i in link[1]:
       yt = YouTube(i)
-      for j in yt.streams:
-          check_res = str(j)
-          if check_res.find(res) > 0:
-              ys = yt.streams.filter(res=res).first()
-              break
-          else:
-              ys = yt.streams.get_highest_resolution()
+      ys = yt.streams.get_highest_resolution()
       filename = ys.download()
-      print("threading 2 -->  " + filename.split('/')[-1] + ' -> Downloaded')
+      print("threading 2 -->  " + filename.split('/')[-1] + ' Downloaded')
 
 def downloader3():
     for i in link[2]:
       yt = YouTube(i)
-      for j in yt.streams:
-          check_res = str(j)
-          if check_res.find(res) > 0:
-              ys = yt.streams.filter(res=res).first()
-              break
-          else:
-              ys = yt.streams.get_highest_resolution()
+      ys = yt.streams.get_highest_resolution()
       filename = ys.download()
-      print("threading 3 -->  " + filename.split('/')[-1] + ' -> Downloaded')
+      print("threading 3 -->  " + filename.split('/')[-1] + ' Downloaded')
 
 def downloader4():
     for i in link[3]:
       yt = YouTube(i)
-      for j in yt.streams:
-          check_res = str(j)
-          if check_res.find(res) > 0:
-              ys = yt.streams.filter(res=res).first()
-              break
-          else:
-              ys = yt.streams.get_highest_resolution()
+      ys = yt.streams.get_highest_resolution()
       filename = ys.download()
-      print("threading 4 -->  " + filename.split('/')[-1] + ' -> Downloaded')
+      print("threading 4 -->  " + filename.split('/')[-1] + ' Downloaded')
 
 t1 = threading.Thread(target=downloader1, name='d1')
 t2 = threading.Thread(target=downloader2,name='d2')
